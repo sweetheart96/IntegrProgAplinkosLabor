f1cca403f676133e8e3e50a7a2a2c8fd610f0c1b
Vitalija <vitalija.rudkevic@stud.vgtu.lt>
2019-03-25 23:10:49 +0200

    file reading function added

---
+++ StudentApp/Service/FileReader.cs
@@ -0,0 +1,25 @@
+﻿using StudentApp.Entity;
+using StudentApp.ViewModels;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace StudentApp.Service
+{
+    public static class FileReader
+    {
+        //Default file  
+        static readonly string textFilePath = "..\\..\\..\\kursiokai.txt";
+
+        public static List<Student> ReadFile()
+        {
+            var students = new List<Student>();
+            string[] lines = File.ReadAllLines(textFilePath);
+            lines = lines.Skip(1).ToArray();
+
+            students.AddRange(FileStudentModel.ToEntity(lines));
+
+            return students;
+        }
+    }
+}
---
+++ StudentApp/ViewModels/FileStudentModel.cs
@@ -0,0 +1,48 @@
+﻿using StudentApp.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace StudentApp.ViewModels
+{
+    public class FileStudentModel
+    {
+        private static Student ToEntity(string lineFromFile)
+        {
+            string[] words = lineFromFile.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            var examMark = Int32.Parse(words[7]);
+            var marks = new List<int>
+            {
+                Int32.Parse(words[2]),
+                Int32.Parse(words[3]),
+                Int32.Parse(words[4]),
+                Int32.Parse(words[5]),
+                Int32.Parse(words[6])
+            };
+
+            var student = Student.Create(words[0], words[1], marks, examMark);
+
+            return student;
+
+        }
+
+        public static List<Student> ToEntity(string[] linesFromFile)
+        {
+            var students = new List<Student>();
+
+            try
+            {
+                foreach(var item in linesFromFile)
+                {
+                    students.Add(ToEntity(item));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ivyko klaida kuriant Student objekta is failo");
+            }
+
+            return students;
+        }
+    }
+}
